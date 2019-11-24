1accf51fa01bdd24cb00ee183d5e97a3f5627f5c
David Dunn <26876072+doubledup@users.noreply.github.com>
2019-11-24 16:59:20 +0200

    Add brew next to native fzf shell setup

--- fzf.zsh
+++ fzf.zsh
@@ -1,13 +1,17 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */Users/david.dunn/.fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/Users/david.dunn/.fzf/bin"
+if [[ -f '~/.fzf/bin' && ! "$PATH" == *~/.fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}~/.fzf/bin"
+elif [[ -f "$(brew --prefix)/opt/fzf/bin" && ! "$PATH" == "*$(brew --prefix)/opt/fzf/bin*" ]]; then
+  export PATH="${PATH:+${PATH}:}$(brew --prefix)/opt/fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/Users/david.dunn/.fzf/shell/completion.zsh" 2> /dev/null
+[[ -f '~/.fzf/shell/completion.zsh' && $- == *i* ]] && source '~/.fzf/shell/completion.zsh' 2> /dev/null
+[[ -f "$(brew --prefix)/opt/fzf/shell/completion.zsh" && $- == *i* ]] && source "$(brew --prefix)/opt/fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/Users/david.dunn/.fzf/shell/key-bindings.zsh"
+[ -f '~/.fzf/shell/key-bindings.zsh' ] && source '~/.fzf/shell/key-bindings.zsh'
+[ -f "$(brew --prefix)/opt/fzf/shell/key-bindings.zsh" ] && source "$(brew --prefix)/opt/fzf/shell/key-bindings.zsh"
