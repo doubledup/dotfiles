ec6f33191ce5721745b28645cb92034683fb9cc3
David Dunn <26876072+doubledup@users.noreply.github.com>
2020-04-13 01:08:40 +0200

    Set up fzf key bindings only

--- fzf.zsh
+++ fzf.zsh
@@ -1,17 +1,17 @@
 # Setup fzf
 # ---------
-if [[ -f '~/.fzf/bin' && ! "$PATH" == *~/.fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}~/.fzf/bin"
-elif [[ -f "$(brew --prefix)/opt/fzf/bin" && ! "$PATH" == "*$(brew --prefix)/opt/fzf/bin*" ]]; then
-  export PATH="${PATH:+${PATH}:}$(brew --prefix)/opt/fzf/bin"
-fi
+# if [[ -f '~/.fzf/bin' && ! "$PATH" == *~/.fzf/bin* ]]; then
+#   export PATH="${PATH:+${PATH}:}~/.fzf/bin"
+# elif [[ -f "$(brew --prefix)/opt/fzf/bin" && ! "$PATH" == "*$(brew --prefix)/opt/fzf/bin*" ]]; then
+#   export PATH="${PATH:+${PATH}:}$(brew --prefix)/opt/fzf/bin"
+# fi
 
 # Auto-completion
 # ---------------
-[[ -f '~/.fzf/shell/completion.zsh' && $- == *i* ]] && source '~/.fzf/shell/completion.zsh' 2> /dev/null
-[[ -f "$(brew --prefix)/opt/fzf/shell/completion.zsh" && $- == *i* ]] && source "$(brew --prefix)/opt/fzf/shell/completion.zsh" 2> /dev/null
+# [[ -f '~/.fzf/shell/completion.zsh' && $- == *i* ]] && source '~/.fzf/shell/completion.zsh' 2> /dev/null
+# [[ -f "$(brew --prefix)/opt/fzf/shell/completion.zsh" && $- == *i* ]] && source "$(brew --prefix)/opt/fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-[ -f '~/.fzf/shell/key-bindings.zsh' ] && source '~/.fzf/shell/key-bindings.zsh'
+[ -f '/usr/share/fzf/key-bindings.zsh' ] && source '/usr/share/fzf/key-bindings.zsh'
 [ -f "$(brew --prefix)/opt/fzf/shell/key-bindings.zsh" ] && source "$(brew --prefix)/opt/fzf/shell/key-bindings.zsh"
