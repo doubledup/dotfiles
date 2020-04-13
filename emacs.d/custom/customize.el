539029ed980e8b0a178cea6380e1852eb955166f
David Dunn <26876072+doubledup@users.noreply.github.com>
2020-04-13 09:54:25 +0200

    Split up Emacs config files

---
+++ emacs.d/custom/customize.el
@@ -0,0 +1,36 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
+ '(blink-cursor-mode nil)
+ '(custom-enabled-themes (quote (solarized-light)))
+ '(custom-file "~/.emacs.d/custom/customize.el")
+ '(custom-safe-themes
+   (quote
+    ("d91ef4e714f05fff2070da7ca452980999f5361209e679ee988e3c432df24347" "0598c6a29e13e7112cfbc2f523e31927ab7dce56ebb2016b567e1eff6dc1fd4f" default)))
+ '(display-line-numbers (quote visual))
+ '(ido-enable-flex-matching t)
+ '(ido-everywhere t)
+ '(ido-mode (quote both) nil (ido))
+ '(indent-tabs-mode nil)
+ '(inhibit-startup-screen t)
+ '(package-archive-priorities (quote (("melpa-stable" . 1))))
+ '(package-archives
+   (quote
+    (("gnu" . "https://elpa.gnu.org/packages/")
+     ("melpa-stable" . "https://stable.melpa.org/packages/"))))
+ '(package-enable-at-startup t)
+ '(package-selected-packages (quote (solarized-theme org)))
+ '(ring-bell-function (quote ignore))
+ '(scroll-bar-mode nil)
+ '(standard-indent 4)
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:height 110 :foundry "SRC" :family "Hack")))))
---
+++ emacs.d/init.el
@@ -0,0 +1,7 @@
+;; Emacs init order:
+;; site-start init default abbrevs packages after-init-time after-init-hook
+
+(package-initialize)
+(load "~/.emacs.d/custom/customize")
+(load "~/.emacs.d/custom/global")
+(load "~/.emacs.d/custom/local")
