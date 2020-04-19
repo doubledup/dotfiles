bb950e98e4ac89c061c0b62fce91b339d8c73808
David Dunn <26876072+doubledup@users.noreply.github.com>
2020-04-19 16:00:28 +0200

    Switch Emacs theme to Zenburn

--- emacs.d/custom/customize.el
+++ emacs.d/custom/customize.el
@@ -3,14 +3,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
  '(blink-cursor-mode nil)
- '(custom-enabled-themes (quote (solarized-light)))
+ '(custom-enabled-themes (quote (zenburn)))
  '(custom-file "~/.emacs.d/custom/customize.el")
  '(custom-safe-themes
    (quote
-    ("d91ef4e714f05fff2070da7ca452980999f5361209e679ee988e3c432df24347" "0598c6a29e13e7112cfbc2f523e31927ab7dce56ebb2016b567e1eff6dc1fd4f" default)))
+    ("84890723510d225c45aaff941a7e201606a48b973f0121cb9bcb0b9399be8cba" default)))
  '(display-line-numbers (quote visual))
  '(ido-enable-flex-matching t)
  '(ido-everywhere t)
@@ -23,17 +21,9 @@
     (("gnu" . "https://elpa.gnu.org/packages/")
      ("melpa-stable" . "https://stable.melpa.org/packages/"))))
  '(package-enable-at-startup t)
- '(package-selected-packages (quote (solarized-theme org)))
+ '(package-selected-packages (quote (zenburn-theme org)))
  '(ring-bell-function (quote ignore))
  '(scroll-bar-mode nil)
- '(solarized-height-minus-1 1)
- '(solarized-height-plus-1 1)
- '(solarized-height-plus-2 1)
- '(solarized-height-plus-3 1)
- '(solarized-height-plus-4 1)
- '(solarized-scale-org-headlines nil)
- '(solarized-scale-outline-headlines nil)
- '(solarized-use-variable-pitch nil)
  '(standard-indent 4)
  '(tool-bar-mode nil))
 (custom-set-faces
