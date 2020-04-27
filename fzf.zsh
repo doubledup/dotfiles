e25ee1a6e12ccf868c0fbb68a4faa880fbaa850d
David Dunn <26876072+doubledup@users.noreply.github.com>
2020-04-27 13:32:55 +0200

    Clean up fzf init file

--- fzf.zsh
+++ fzf.zsh
@@ -1,14 +1,12 @@
 # Setup fzf
 # ---------
-# if [[ -f '~/.fzf/bin' && ! "$PATH" == *~/.fzf/bin* ]]; then
-#   export PATH="${PATH:+${PATH}:}~/.fzf/bin"
 if [[ -f "$(brew --prefix)/opt/fzf/bin" && ! "$PATH" == "*$(brew --prefix)/opt/fzf/bin*" ]]; then
   export PATH="${PATH:+${PATH}:}$(brew --prefix)/opt/fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-# [[ -f '~/.fzf/shell/completion.zsh' && $- == *i* ]] && source '~/.fzf/shell/completion.zsh' 2> /dev/null
+[[ -f '/usr/share/fzf/completion.zsh' && $- == *i* ]] && source '/usr/share/fzf/completion.zsh' 2> /dev/null
 [[ -f "$(brew --prefix)/opt/fzf/shell/completion.zsh" && $- == *i* ]] && source "$(brew --prefix)/opt/fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
