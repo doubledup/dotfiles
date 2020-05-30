dc11f13a17ce23f3f2c4472b28a5c8f6582a25a5
David Dunn <26876072+doubledup@users.noreply.github.com>
2020-05-30 12:01:18 +0200

    Separate fzf config by OS

---
+++ tag-linux/fzf.zsh
@@ -0,0 +1,7 @@
+# Auto-completion
+# ---------------
+[[ -f '/usr/share/fzf/completion.zsh' && $- == *i* ]] && source '/usr/share/fzf/completion.zsh' 2> /dev/null
+
+# Key bindings
+# ------------
+[ -f '/usr/share/fzf/key-bindings.zsh' ] && source '/usr/share/fzf/key-bindings.zsh'
--- tag-mac/fzf.zsh
+++ tag-mac/fzf.zsh
@@ -6,10 +6,8 @@ fi
 
 # Auto-completion
 # ---------------
-[[ -f '/usr/share/fzf/completion.zsh' && $- == *i* ]] && source '/usr/share/fzf/completion.zsh' 2> /dev/null
 [[ -f "$(brew --prefix)/opt/fzf/shell/completion.zsh" && $- == *i* ]] && source "$(brew --prefix)/opt/fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-[ -f '/usr/share/fzf/key-bindings.zsh' ] && source '/usr/share/fzf/key-bindings.zsh'
 [ -f "$(brew --prefix)/opt/fzf/shell/key-bindings.zsh" ] && source "$(brew --prefix)/opt/fzf/shell/key-bindings.zsh"
