e13ce7c85cf029427210dcf4bc3c8672a67dd2cd
David Dunn <26876072+doubledup@users.noreply.github.com>
2020-06-04 08:54:35 +0200

    Update Emacs safe themes

--- emacs.d/customize.el
+++ emacs.d/customize.el
@@ -7,7 +7,7 @@
  '(custom-file "~/.emacs.d/customize.el")
  '(custom-safe-themes
    (quote
-    ("6b2c6e5bc1e89cf7d927d17f436626eac98a04fdab89e080f4e193f6d291c93d" default))))
+    ("84890723510d225c45aaff941a7e201606a48b973f0121cb9bcb0b9399be8cba" "6b2c6e5bc1e89cf7d927d17f436626eac98a04fdab89e080f4e193f6d291c93d" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
