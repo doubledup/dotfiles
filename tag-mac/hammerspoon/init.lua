61ba249323dc8be24eaed89f50d20f298ccc3ae5
David Dunn <26876072+doubledup@users.noreply.github.com>
2021-11-22 07:40:19 +0200

    Set up window switching and remove control binding

--- tag-mac/hammerspoon/init.lua
+++ tag-mac/hammerspoon/init.lua
@@ -2,17 +2,37 @@
 --   hs.alert.show("Hello World!")
 -- end)
 
-hs.hotkey.bind({"ctrl"}, ";", function()
-    local app = hs.application.get("kitty")
-    if app then
-        if not app:mainWindow() then
-            app:selectMenuItem({"kitty", "New OS window"})
-        elseif app:isFrontmost() then
-            app:hide()
-        else
-            app:activate()
-        end
-    else
-        hs.application.launchOrFocus("kitty")
-    end
+-- hs.hotkey.bind({"ctrl"}, ";", function()
+--     local app = hs.application.get("kitty")
+--     if app then
+--         if not app:mainWindow() then
+--             app:selectMenuItem({"kitty", "New OS window"})
+--         elseif app:isFrontmost() then
+--             app:hide()
+--         else
+--             app:activate()
+--         end
+--     else
+--         hs.application.launchOrFocus("kitty")
+--     end
+-- end)
+
+hs.urlevent.bind("openKitty", function(eventName, params)
+    hs.application.launchOrFocus("Kitty")
+end)
+
+hs.urlevent.bind("openBrave", function(eventName, params)
+    hs.application.launchOrFocus("Brave Browser")
+end)
+
+hs.urlevent.bind("openSlack", function(eventName, params)
+    hs.application.launchOrFocus("Slack")
+end)
+
+hs.urlevent.bind("openSpotify", function(eventName, params)
+    hs.application.launchOrFocus("Spotify")
+end)
+
+hs.urlevent.bind("openSignal", function(eventName, params)
+    hs.application.launchOrFocus("Signal")
 end)
