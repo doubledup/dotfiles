73b6bf8f1361d684a6c660012d09a3d121a480cd
David Dunn <26876072+doubledup@users.noreply.github.com>
2022-04-13 20:12:28 +0200

    Add bat to apt installation

--- tag-linux/config/fish/config.os.fish
+++ tag-linux/config/fish/config.os.fish
@@ -14,6 +14,10 @@ alias ls='ls --color=auto'
 alias la='ls -al --color=auto'
 alias ll='ls -l --color=auto'
 
+if type -q batcat
+  alias bat=batcat
+end
+
 alias fd=fdfind
 
 source ~/.asdf/asdf.fish
