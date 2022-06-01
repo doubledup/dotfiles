c7a1eca93c6725da61040498ddb7c6d5500b1bb7
David Dunn <26876072+doubledup@users.noreply.github.com>
2022-06-01 11:13:09 +0200

    Fix fd name

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -18,8 +18,8 @@ set FZF_KEYBINDINGS 'ctrl-o:toggle-all,alt-o:toggle-preview,alt-n:preview-page-d
 set FZF_PREVIEW 'bat --style=numbers --color=always --line-range :1000 {} 2> /dev/null'
 set FZF_PREVIEW_WINDOW 'right:60%:wrap:hidden' #:noborder
 set -x FZF_DEFAULT_OPTS "--height 70% --bind $FZF_KEYBINDINGS --preview '$FZF_PREVIEW' --preview-window '$FZF_PREVIEW_WINDOW'"
-set -x FZF_DEFAULT_COMMAND 'fdfind --type f --hidden --follow --exclude .git'
-set -x FZF_CTRL_T_COMMAND "fdfind --type f --hidden --follow --exclude .git . \$dir"
+set -x FZF_DEFAULT_COMMAND 'fd --type f --hidden --follow --exclude .git'
+set -x FZF_CTRL_T_COMMAND "fd --type f --hidden --follow --exclude .git . \$dir"
 
 ## git
 # The default less flags that git uses are FRX:
