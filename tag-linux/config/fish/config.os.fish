647082bbe46160f2e4ce86c0775ebc618e112769
David Dunn <26876072+doubledup@users.noreply.github.com>
2022-06-18 16:42:05 +0200

    Remove comments

--- tag-linux/config/fish/config.os.fish
+++ tag-linux/config/fish/config.os.fish
@@ -1,15 +1,5 @@
 #!/usr/bin/env fish
 
-# # get extra info for VTE-based terminal emulators
-# # (lets new terminals open with the same dir as the current one)
-# if [ $VTE_VERSION ]; then
-#   if [ -f /etc/profile.d/vte.sh ]; then
-#     source /etc/profile.d/vte.sh
-#   elif [ -f /usr/share/defaults/etc/profile.d/vte.sh ]; then
-#     source /usr/share/defaults/etc/profile.d/vte.sh
-#   fi
-# fi
-
 [ -f ~/.asdf/asdf.fish ]; and source ~/.asdf/asdf.fish
 
 set fzf_key_bindings_file /usr/share/fish/vendor_functions.d/fzf_key_bindings.fish
--- tag-mac/config/fish/config.os.fish
+++ tag-mac/config/fish/config.os.fish
@@ -1,16 +1,7 @@
 #!/usr/bin/env fish
 
-# ARM Homebrew binaries
 [ -f /opt/homebrew/bin/brew ]; and eval (/opt/homebrew/bin/brew shellenv)
 
 [ -f (/opt/homebrew/bin/brew --prefix asdf)/asdf.fish ]; and source (/opt/homebrew/bin/brew --prefix asdf)/asdf.fish
 
 set -x GOKU_EDN_CONFIG_FILE "$HOME/.config/goku.edn"
-
-# zsh plugins
-# if [ -f "$(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" ]; then
-#   source "$(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
-# fi
-# if [ -f "$(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh" ]; then
-#   source "$(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
-# fi
