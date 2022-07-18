a6f97132c1a3dd30fcf70539452f8abb2103d4af
David Dunn <26876072+doubledup@users.noreply.github.com>
2022-07-18 10:18:34 +0200

    Use Nerd font in Emacs

--- emacs.d/customize.el
+++ emacs.d/customize.el
@@ -13,4 +13,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:height 110 :foundry "SRC" :family "Hack")))))
+ '(default ((t (:height 110 :foundry "SRC" :family "Hack Nerd Font Mono")))))
