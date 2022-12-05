a8fb965c320b61f5e70d86ec2be2cabe0e91c2f1
David Dunn <26876072+doubledup@users.noreply.github.com>
2022-12-05 14:45:16 +0200

    Clean up hyper key mapping

--- tag-mac/hammerspoon/init.lua
+++ tag-mac/hammerspoon/init.lua
@@ -1,28 +1,67 @@
 require('util')
 
-MEH = {"cmd", "alt", "ctrl"}
-HYPER = {"cmd", "alt", "ctrl", "shift"}
-
+-- TODO: https://www.hammerspoon.org/Spoons/MicMute.html
 -- TODO: add keyboard backlight bindings
 
-hs.hotkey.bind(MEH, ".", function()
-  hs.reload()
-end)
+local meh = {"cmd", "alt", "ctrl"}
+local hyper = {"cmd", "alt", "ctrl", "shift"}
+
+SetRightCmdToHyper()
+
+-- hyper = hs.hotkey.modal.new({}, 'F20')
+-- function enterHyperMode()
+--     hs.alert.show("ENTER")
+--     hyper.triggered = false
+--     hyper:enter()
+-- end
+-- -- send ESCAPE if no other keys are pressed.
+-- function exitHyperMode()
+--     hs.alert.show("EXIT")
+--     hyper:exit()
+--     -- if not hyper.triggered then
+--     --     hs.eventtap.keyStroke({}, 'ESCAPE')
+--     -- end
+-- end
+-- hs.hotkey.bind({}, 'F20', enterHyperMode, exitHyperMode)
+
+-- hyper:bind({}, ".", function ()
+--     hs.reload()
+-- end)
+
+-- hyper:bind({}, "b", function ()
+--     hs.execute("/opt/homebrew/bin/blueutil --power toggle")
+-- end)
+-- hyper:bind({}, "c", function ()
+--     hs.execute("ID=$(/opt/homebrew/bin/blueutil --paired | grep 'WH-1000XM3' | sed -E 's/^address: ([0-9a-f]{2}-[0-9a-f]{2}-[0-9a-f]{2}-[0-9a-f]{2}-[0-9a-f]{2}-[0-9a-f]{2}).*/\\1/'); /opt/homebrew/bin/blueutil --connect $ID")
+-- end)
+
+-- hyper:bind({}, "j", function ()
+--     hs.application.launchOrFocus("Brave Browser")
+-- end)
+-- hyper:bind({}, "k", function ()
+--     hs.application.launchOrFocus("Kitty")
+-- end)
+
+
 hs.notify.new({title = "Hammerspoon", informativeText = "Config loaded"}):send()
 
-hs.hotkey.bind(HYPER, "b", "Bluetooth toggle", function(eventName, params)
+hs.hotkey.bind(meh, ".", function()
+    hs.reload()
+end)
+
+hs.hotkey.bind(hyper, "b", "Bluetooth toggle", function(eventName, params)
     hs.execute("/opt/homebrew/bin/blueutil --power toggle")
 end)
-hs.hotkey.bind(HYPER, "c", "Bluetooth connect", function(eventName, params)
+hs.hotkey.bind(hyper, "c", "Bluetooth connect", function(eventName, params)
     hs.execute("ID=$(/opt/homebrew/bin/blueutil --paired | grep 'WH-1000XM3' | sed -E 's/^address: ([0-9a-f]{2}-[0-9a-f]{2}-[0-9a-f]{2}-[0-9a-f]{2}-[0-9a-f]{2}-[0-9a-f]{2}).*/\\1/'); /opt/homebrew/bin/blueutil --connect $ID")
 end)
 
-HyperPickWindow("j", "Brave Browser")
-HyperPickWindow("k", "Kitty")
-HyperPickWindow("o", "Visual Studio Code")
-HyperPickWindow("l", "Slack")
-HyperPickWindow("m", "Signal")
-HyperPickWindow("u", "Spotify")
-HyperPickWindow(";", "Emacs")
+HyperPickWindow(hyper, "j", "Brave Browser")
+HyperPickWindow(hyper, "k", "Kitty")
+HyperPickWindow(hyper, "o", "Visual Studio Code")
+HyperPickWindow(hyper, "l", "Slack")
+HyperPickWindow(hyper, "m", "Signal")
+HyperPickWindow(hyper, "u", "Spotify")
+HyperPickWindow(hyper, ";", "Emacs")
 
 SetRightCmdToHyper()
--- tag-mac/hammerspoon/util.lua
+++ tag-mac/hammerspoon/util.lua
@@ -1,5 +1,5 @@
-function HyperPickWindow(key, appName)
-    hs.hotkey.bind(HYPER, key, appName, function()
+function HyperPickWindow(modifier, key, appName)
+    hs.hotkey.bind(modifier, key, appName, function()
         hs.application.launchOrFocus(appName)
     end)
 end
@@ -8,14 +8,18 @@ function SetRightCmdToHyper()
     -- right cmd as hyper, cribbed from https://github.com/Hammerspoon/hammerspoon/issues/1025#issuecomment-1011435207
     -- we have to watch for flag changes *and* keyDown/Up since a command key (e.g. Cmd-C) is
     -- it's own event
-    hs.eventtap.new({hs.eventtap.event.types.keyDown}, function(ev)
+    hs.eventtap.new({
+        -- hs.eventtap.event.types.flagsChanged,
+        -- hs.eventtap.event.types.keyUp,
+        hs.eventtap.event.types.keyDown}, function(ev)
         -- synthesized events set 0x20000000 and we may or may not get the nonCoalesced bit,
         -- so filter them out
         local rawFlags = ev:getRawEventData().CGEventData.flags & 0xdffffeff
-        local regularFlags = ev:getFlags()
 
         -- uncomment this out when troubleshooting -- apparently different modifiers use
         -- different flags indicating left vs right: e.g.
+        -- local regularFlags = ev:getFlags()
+        -- print(inspect(regularFlags), rawFlags)
 
         --     {
         --       cmd = true
@@ -34,11 +38,8 @@ function SetRightCmdToHyper()
         --       shift = true
         --     }    131074
         --     {}   0
-        --
-        -- etc.
-        --     print(inspect(regularFlags), rawFlags)
 
-        if rawFlags == 1048592 and ev:getType() == hs.eventtap.event.types.keyDown then
+        if rawFlags == 1048592 then
             ev:setFlags({
                 cmd = true,
                 alt = true,
@@ -47,5 +48,19 @@ function SetRightCmdToHyper()
             })
             return true, { ev }
         end
+
+        -- if rawFlags == 1048592 then
+        --     if ev:getType() == hs.eventtap.event.types.keyDown then
+        --         ev:setFlags({
+        --             cmd = true,
+        --             alt = true,
+        --             shift = true,
+        --             ctrl = true,
+        --         })
+        --         return true, { ev }
+        --     else
+        --         return true, {}
+        --     end
+        -- end
     end):start()
 end
