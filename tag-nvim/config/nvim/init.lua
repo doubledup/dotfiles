f413c059bf30babbff09bad1f058da00a9107714
David Dunn <26876072+doubledup@users.noreply.github.com>
2023-10-26 09:52:07 +0200

    Navigate diffs instead of changes

--- tag-nvim/config/nvim/init.lua
+++ tag-nvim/config/nvim/init.lua
@@ -638,14 +638,14 @@ require('gitsigns').setup({
         end
 
         -- Navigation
-        map('n', ']c', function()
-            if vim.wo.diff then return ']c' end
+        map('n', ']d', function()
+            if vim.wo.diff then return ']d' end
             vim.schedule(function() gs.next_hunk() end)
             return '<Ignore>'
         end, { expr = true })
 
-        map('n', '[c', function()
-            if vim.wo.diff then return '[c' end
+        map('n', '[d', function()
+            if vim.wo.diff then return '[d' end
             vim.schedule(function() gs.prev_hunk() end)
             return '<Ignore>'
         end, { expr = true })
