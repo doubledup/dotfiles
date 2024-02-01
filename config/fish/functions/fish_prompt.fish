449253718ab18efa97b75228dfd3e43824170ef1
David Dunn <26876072+doubledup@users.noreply.github.com>
2024-02-01 18:02:26 +0200

    Plain prompt. No snazz.

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -66,7 +66,7 @@ function fish_prompt --description 'Write out the prompt'
             set suffix '#'
         case '*'
             set color_cwd $fish_color_cwd
-            set suffix '♠'
+            set suffix '$'
     end
 
     # PWD
