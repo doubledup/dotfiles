0a646417c6d432b78a06fdee03e6c27c5de1ca7a
David Dunn <26876072+doubledup@users.noreply.github.com>
2024-10-14 10:49:58 +0200

    Remove goku config

--- tag-mac/config/fish/config.os.fish
+++ tag-mac/config/fish/config.os.fish
@@ -1,8 +1,6 @@
 #!/usr/bin/env fish
 
-set -gx PNPM_HOME "/Users/daviddunn/Library/pnpm"
-set -gx PATH "$PNPM_HOME" $PATH
-
-set -x GOKU_EDN_CONFIG_FILE "$HOME/.config/goku.edn"
+# set -gx PNPM_HOME "/Users/daviddunn/Library/pnpm"
+# set -gx PATH "$PNPM_HOME" $PATH
 
 [ -z "$IN_NIX_SHELL" -a -f /opt/homebrew/bin/brew ]; and eval (/opt/homebrew/bin/brew shellenv)
