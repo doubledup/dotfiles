df1939d1b575d7f7c75fd97dae7996cf79336b32
David Dunn <26876072+doubledup@users.noreply.github.com>
2025-02-16 16:18:26 +0200

    Set up fzf and JAVA_HOME on Mac

--- tag-mac/config/fish/config.os.fish
+++ tag-mac/config/fish/config.os.fish
@@ -1,6 +1,7 @@
 #!/usr/bin/env fish
 
-# set -gx PNPM_HOME "/Users/daviddunn/Library/pnpm"
-# set -gx PATH "$PNPM_HOME" $PATH
-
 [ -z "$IN_NIX_SHELL" -a -f /opt/homebrew/bin/brew ]; and eval (/opt/homebrew/bin/brew shellenv)
+
+fzf --fish | source
+
+set -x JAVA_HOME /Library/Java/JavaVirtualMachines/amazon-corretto-21.jdk/Contents/Home/
