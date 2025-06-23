4f94acfa697db2b8d9a8b37b514416eb842a8ecc
David Dunn <26876072+doubledup@users.noreply.github.com>
2025-06-23 20:45:20 +0300

    Remove default slime mappings

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -222,7 +222,14 @@ require("lazy").setup({
     'andrewradev/linediff.vim',
     { 'smoka7/hop.nvim', opts = {}, },
     'honza/vim-snippets',
-    'jpalardy/vim-slime',
+
+    {
+        "jpalardy/vim-slime",
+        init = function()
+            vim.g.slime_no_mappings = 1
+        end
+    },
+
     'mizlan/iswap.nvim',
     { 'numToStr/Comment.nvim', opts = {}, lazy = false, },
 
