dd04bbcae6a6b0d5792c385874e888f6c879bb9a
David Dunn <26876072+doubledup@users.noreply.github.com>
2025-07-12 18:16:09 +0200

    Add noice notifications & snacks for bigfile

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -104,6 +104,25 @@ require("lazy").setup({
             end
         },
 
+        {
+            'folke/noice.nvim',
+            event = 'VeryLazy',
+            opts = {
+                cmdline = { enabled = false, },
+                messages = { enabled = false }
+            },
+            dependencies = {
+                -- if you lazy-load any plugin below, make sure to add proper `module='...'` entries
+                'MunifTanjim/nui.nvim',
+                'rcarriga/nvim-notify',
+            },
+        },
+        {
+            'folke/snacks.nvim',
+            opts = {
+                bigfile = {}
+            }
+        },
         'folke/which-key.nvim',
         'junegunn/fzf',
         'junegunn/fzf.vim',
