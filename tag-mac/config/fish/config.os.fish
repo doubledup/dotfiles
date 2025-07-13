ed43dd204bad35b4ee8719ed6de1caac4f25b4fb
David Dunn <26876072+doubledup@users.noreply.github.com>
2025-07-13 23:25:45 +0200

    Remove default $JAVA_HOME

--- tag-mac/config/fish/config.os.fish
+++ tag-mac/config/fish/config.os.fish
@@ -3,5 +3,3 @@
 [ -z "$IN_NIX_SHELL" -a -f /opt/homebrew/bin/brew ]; and eval (/opt/homebrew/bin/brew shellenv)
 
 fzf --fish | source
-
-set -x JAVA_HOME /Library/Java/JavaVirtualMachines/amazon-corretto-21.jdk/Contents/Home/
