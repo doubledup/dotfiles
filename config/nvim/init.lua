af4a048d94396b2503853b9ff349587246f9ab31
David Dunn <26876072+doubledup@users.noreply.github.com>
2025-07-13 19:55:50 +0200

    Allow noice at bottom-right with classic cmdline

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -107,14 +107,16 @@ require('lazy').setup({
         {
             'folke/noice.nvim',
             event = 'VeryLazy',
-            opts = {
-                cmdline = { enabled = false, },
-                messages = { enabled = false }
-            },
             dependencies = {
                 -- if you lazy-load any plugin below, make sure to add proper `module='...'` entries
                 'MunifTanjim/nui.nvim',
-                'rcarriga/nvim-notify',
+                {
+                    'rcarriga/nvim-notify',
+                    opts = { top_down = false }
+                },
+            },
+            opts = {
+                cmdline = { view = 'cmdline' },
             },
         },
         {
