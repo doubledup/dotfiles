1c43a145d9f4fec5093f65682ab21d6020dc22e5
David Dunn <26876072+doubledup@users.noreply.github.com>
2025-08-24 08:35:23 +0200

    Clean up main fish config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,13 +1,19 @@
-# Environment variables
+set -U fish_greeting
+set fish_prompt_pwd_full_dirs 2
 
-set -x BAT_PAGER "less $LESS"
-set -x BAT_THEME ayu-mirage
-set -x BAT_STYLE changes,header-filename
+# env
 
 set -x EDITOR nvim
-set -U fish_greeting
+# abbr kept here so it has access to EDITOR (aliases.fish loads before config.fish)
+abbr v "$EDITOR"
+abbr vk 'NVIM_APPNAME=nvim-kickstart nvim'
+abbr nvim! 'nvim -N -u NONE -U NONE'
 
-## less
+set -x BAT_PAGER "less $LESS"
+set -x BAT_STYLE changes,header-filename
+set -x BAT_THEME ayu-mirage
+
+# less
 # -N: show line numbers
 # -Q: keep less quiet, so the terminal bell is never rung. Use a visual bell if it exists.
 # -R: output ANSI colors and OSC 8 hyperlinks in "raw" form, allowing the terminal emulator to
@@ -17,41 +23,28 @@ set -U fish_greeting
 set -x LESS -NQRix4
 set -x LESSOPEN '|bat --color=always --paging=never %s'
 
-## Path
+# path
 
-### binaries
 set -x PATH $HOME/.local/bin $PATH
 
-### dotnet
 set -x DOTNET_CLI_TELEMETRY_OPTOUT 1
 set -x PATH $HOME/.dotnet/tools $PATH
 
-### homebrew
 set -x HOMEBREW_NO_ANALYTICS 1
 
-### go
 set -x GOPATH "$HOME/.local/share/go"
 set -x PATH $GOPATH/bin $PATH
 
-### sam
 set -x SAM_CLI_TELEMETRY 0
 
-set fish_prompt_pwd_full_dirs 2
-
-# EDITOR / {neo,}vim abbrs
-# kept here so they have access to EDITOR (aliases.fish loads before config.fish)
-abbr v "$EDITOR"
-abbr vim! 'vim -N -u NONE -U NONE'
-abbr nvim! 'nvim -N -u NONE -U NONE'
-abbr vk 'NVIM_APPNAME=nvim-kickstart nvim'
+# other
 
 test -e ~/.config/fish/config.os.fish; and source ~/.config/fish/config.os.fish
 test -e ~/.config/fish/config.local.fish; and source ~/.config/fish/config.local.fish
 
-if type -q bat
-    set -x PAGER "$(command -v bat)"
-end
-
-# zoxide needs to be rerun in new shells
-[ -f (command -v zoxide) ]; and zoxide init --cmd j fish | source
-[ -z "$IN_NIX_SHELL" -a -f (command -v direnv) ]; and direnv hook fish | source
+# these run after the OS settings so that we know the commands are available.
+# e.g. `brew shellenv` on MacOS sets these up in PATH.
+set -x PAGER "$(command -v bat)"
+[ -z "$IN_NIX_SHELL" ]; and direnv hook fish | source
+fzf --fish | source
+zoxide init --cmd j fish | source
--- tag-mac/config/fish/config.os.fish
+++ tag-mac/config/fish/config.os.fish
@@ -1,3 +1,1 @@
 [ -z "$IN_NIX_SHELL" -a -f /opt/homebrew/bin/brew ]; and eval (/opt/homebrew/bin/brew shellenv)
-
-fzf --fish | source
