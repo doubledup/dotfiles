eff14e664f7be3a3a1810a178818a5172892c91e
David Dunn <26876072+doubledup@users.noreply.github.com>
2025-08-31 20:16:43 +0200

    Add vim-go to plugins module

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -268,17 +268,6 @@ vim.filetype.add({
 
 -- plugins
 
--- go
-vim.g.go_code_completion_enabled = 0
-vim.g.go_doc_keywordprg_enabled = 0
-vim.g.go_fmt_autosave = 0
-vim.g.go_def_mapping_enabled = 0
-vim.g.go_term_mode = "split"
-vim.g.go_term_reuse = 1
-vim.g.go_term_enabled = 1
-vim.g.go_term_close_on_exit = 0
-vim.g.go_gopls_enabled = 0
-
 -- hop
 local hop = require("hop")
 vim.keymap.set("n", "s", function()
--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -231,9 +231,27 @@ return {
     -- included for folding
     -- "preservim/vim-markdown",
     -- "elixir-tools/elixir-tools.nvim"
-    -- {
-    --     "fatih/vim-go", build = ":GoUpdateBinaries"
-    -- },
+
+    {
+        "fatih/vim-go",
+        build = ":GoUpdateBinaries",
+        ft = "go",
+        init = function()
+            -- Disable features handled by LSP
+            vim.g.go_code_completion_enabled = 0
+            vim.g.go_doc_keywordprg_enabled = 0
+            vim.g.go_fmt_autosave = 0
+            vim.g.go_def_mapping_enabled = 0
+            vim.g.go_gopls_enabled = 0
+
+            -- Terminal settings
+            vim.g.go_term_mode = "split"
+            vim.g.go_term_reuse = 1
+            vim.g.go_term_enabled = 1
+            vim.g.go_term_close_on_exit = 0
+        end,
+    },
+
     "ChrisWellsWood/roc.vim",
 
     -- as needed
