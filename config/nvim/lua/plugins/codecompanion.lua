cabde44ef7c1521cba0b5ea2bab05a84672bd6b2
David Dunn <26876072+doubledup@users.noreply.github.com>
2026-01-27 08:53:46 +0100

    Update codecompanion version

--- config/nvim/lua/plugins/codecompanion.lua
+++ config/nvim/lua/plugins/codecompanion.lua
@@ -1,6 +1,6 @@
 return {
     "olimorris/codecompanion.nvim",
-    version = "v17.33.0",
+    version = "^18.0.0",
     dependencies = {
         "nvim-lua/plenary.nvim",
         "nvim-treesitter/nvim-treesitter",
