6783e7e795cc865c6ad2f9eb3129558094111c5e
David Dunn <26876072+doubledup@users.noreply.github.com>
2026-04-02 11:41:34 +0200

    Expand linters to fish, tf and yaml

--- config/nvim/lua/plugins/nvim-lint.lua
+++ config/nvim/lua/plugins/nvim-lint.lua
@@ -5,8 +5,12 @@ return {
     config = function()
         local lint = require("lint")
 
+        -- sh/bash: bashls runs shellcheck (from PATH) internally
         lint.linters_by_ft = {
+            fish = { "fish" },
             sql = { "sqlfluff" },
+            terraform = { "tflint" },
+            yaml = { "yamllint" },
         }
 
         vim.api.nvim_create_autocmd({ "BufReadPost", "BufWritePost" }, {
