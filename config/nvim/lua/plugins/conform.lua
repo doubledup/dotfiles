855e5f99f94dc1843e16116e820f4fe635ff0705
David Dunn <26876072+doubledup@users.noreply.github.com>
2026-04-02 09:16:12 +0200

    Enable conform's format_on_save

--- config/nvim/lua/plugins/conform.lua
+++ config/nvim/lua/plugins/conform.lua
@@ -13,54 +13,15 @@ return {
                 require("conform").format({ async = true, lsp_format = "fallback" })
             end,
         },
-
-        -- {
-        --     "<cr>",
-        --     mode = "n",
-        --     desc = "Format and save all buffers (except in quickfix)",
-        --
-        --     function()
-        --         if vim.bo.buftype == "quickfix" then
-        --             -- In quickfix, leave <cr> behavior as-is
-        --
-        --             vim.api.nvim_feedkeys(
-        --                 vim.api.nvim_replace_termcodes("<cr>", true, false, true),
-        --                 "n",
-        --                 false
-        --             )
-        --         else
-        --             vim.cmd.checktime()
-        --
-        --             -- Format all buffers
-        --             local conform = require("conform")
-        --             for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-        --                 if vim.api.nvim_buf_is_loaded(buf) and vim.bo[buf].modifiable then
-        --                     conform.format({ async = false, lsp_format = "fallback", bufnr = buf })
-        --                 end
-        --             end
-        --
-        --             vim.cmd.wall()
-        --         end
-        --     end,
-        -- },
     },
 
     opts = {
         notify_on_error = false,
 
-        -- format_on_save = function(bufnr)
-        --     -- Disable "format_on_save lsp_fallback" for languages that don't
-        --     -- have a well standardized coding style.
-        --     local disable_filetypes = { c = true, cpp = true }
-        --     if disable_filetypes[vim.bo[bufnr].filetype] then
-        --         return nil
-        --     else
-        --         return {
-        --             timeout_ms = 500,
-        --             lsp_format = "fallback",
-        --         }
-        --     end
-        -- end,
+        format_on_save = {
+            timeout_ms = 500,
+            lsp_format = "fallback",
+        },
 
         formatters_by_ft = {
             fish = { "fish_indent" },
