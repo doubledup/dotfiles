b90671a170228d2d6f4ba1f6af4668774480a715
David Dunn <26876072+doubledup@users.noreply.github.com>
2026-07-14 18:29:13 +0200

    Fix brew shellenv for non-fish login shells
    Without a shell arg, `brew shellenv` emits POSIX syntax when $SHELL is
    not fish (headless just test, CI), breaking fish's eval. Force fish.

--- tag-mac/config/fish/config.os.fish
+++ tag-mac/config/fish/config.os.fish
@@ -1,3 +1,3 @@
-[ -z "$IN_NIX_SHELL" -a -f /opt/homebrew/bin/brew ]; and eval (/opt/homebrew/bin/brew shellenv)
+[ -z "$IN_NIX_SHELL" -a -f /opt/homebrew/bin/brew ]; and eval (/opt/homebrew/bin/brew shellenv fish)
 
 set -x HOMEBREW_REQUIRE_TAP_TRUST 1
