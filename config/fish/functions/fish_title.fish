cec51548bd21dd4ce4139d9bc5b44507d3238705
David Dunn <26876072+doubledup@users.noreply.github.com>
2026-08-01 10:50:04 +0200

    Trim whitespace from fish title

--- config/fish/functions/fish_title.fish
+++ config/fish/functions/fish_title.fish
@@ -1,5 +1,5 @@
 function fish_title --description "Set terminal window title"
     set -q argv[1]; or set argv fish
     set command_name (string split ' ' $argv[1])[1]
-    echo $command_name:(prompt_pwd | xargs basename)
+    echo $command_name:"$(prompt_pwd | xargs basename | tr \n ' ')"
 end
